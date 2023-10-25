87007f28846d2860f19162f904838dc7b05818d6
+ <+>
2023-10-25 18:58:36 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
 
+import React,{useState} from "react";
+import "../index.css";
 function Item({ name, category }) {
+  const [cart,setCart]= useState(false);
+ 
+  function handleCart(){
+   setCart(!cart);
+
+    }
+    const cartStatus=cart ? "in-cart":"";
+
+  
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>Add to Cart</button>
     </li>
   );
 }
